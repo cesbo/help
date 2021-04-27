09520c162b202af53affb350273de4796f6f65fd
Andrey Dyldin <and@cesbo.com>
2021-04-27 13:09:18 +0300

    fuse.js for search

--- static/assets/js/search.js
+++ static/assets/js/search.js
@@ -1,6 +1,22 @@
 (function () {
-"use strict";
+'use strict';
 
-fetch()
+function init(data) {
+    window.fuse = new Fuse(data, {
+        keys: ['title', 'content'],
+        includeMatches: true,
+    });
+}
+
+// TODO: start fuse only in searchbox focus
+
+// TODO: make index with `Fuse.createIndex(['title', 'content'], data)`
+//       store index to localstorage
+//       if response from cache use index from localstorage
+
+fetch('/' + document.documentElement.lang + '/index.json')
+    .then(response => { console.log(response); response.json() })
+    .then(json => init(json))
+    .catch(error => console.error(error));
 
 })();
