081171baf567dca1380919ea25ac647be509eda7
Andrey Dyldin <and@cesbo.com>
2021-05-05 15:54:44 +0300

    save lang to cookie

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -1,7 +1,4 @@
-(function() {
-'use strict';
-
-window.$ = function(x) {
+function $(x) {
     if(typeof(x) === 'string') {
         return document.querySelectorAll(x)
     } else if(typeof(x) === 'function') {
@@ -16,13 +13,13 @@ window.$ = function(x) {
     }
 }
 
-$.setCookie = function(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays) {
     const d = new Date()
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
     document.cookie = cname + '=' + cvalue + ';expires=' + d.toUTCString() + ';path=/'
 }
 
-$.getCookie = function(cname) {
+function getCookie(cname) {
     const name = cname + '='
     const ca = decodeURIComponent(document.cookie).split(';')
     for(var i = 0; i < ca.length; i++) {
@@ -37,4 +34,22 @@ $.getCookie = function(cname) {
     return ''
 }
 
-})();
+function checkLanguage(lang) {
+    const list = ['en', 'ru'];
+    if(!!lang) {
+        lang = lang.substring(0, 2);
+        return (list.indexOf(lang) != -1) ? lang : list[0];
+    } else {
+        return list[0];
+    }
+}
+
+function getLanguage(lang) {
+    lang = lang || getCookie('lang') || navigator.language;
+	return checkLanguage(lang);
+}
+
+function setLanguage(lang) {
+    lang = checkLanguage(lang);
+    setCookie('lang', lang, 365);
+}
