9f2d3aeae45a352e95d5969ab7a6492b119ef44f
Andrey Dyldin <and@cesbo.com>
2021-05-12 22:12:20 +0300

    move icons to css; search button in sidebar for docs

--- static/assets/js/search.js
+++ static/assets/js/search.js
@@ -1,6 +1,22 @@
 (function () {
 'use strict';
 
+
+window.startSearch = function(event) {
+    event.preventDefault();
+    window.scrollTo(0, 0);
+
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    document.body.appendChild(modal);
+
+    document.addEventListener('keydown', function(event) {
+
+    })
+}
+
+
 function init(data) {
     window.fuse = new Fuse(data, {
         keys: ['title', 'content'],
@@ -8,11 +24,25 @@ function init(data) {
     });
 }
 
+
 $(function() {
     fetch('/' + document.documentElement.lang + '/index.json')
         .then(response => { console.log(response); response.json() })
         .then(json => init(json))
         .catch(error => console.error(error));
+
+    document.addEventListener('keydown', function(event) {
+        if(!(
+            document.activeElement != document.body ||
+            event.altKey ||
+            event.ctrlKey ||
+            event.metaKey ||
+            event.shiftKey ||
+            event.key != 's'
+        )) {
+            startSearch(event)
+        }
+    })
 });
 
 })();
