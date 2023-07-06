cb9ec31651ebbcbd9436c8a4d71d0cc9e59090d5
Andrey Dyldin <and@cesbo.com>
2023-07-06 17:38:46 +0300

    SiteCategory: refactoring

---
+++ components/SiteCategory.vue
@@ -0,0 +1,63 @@
+<template>
+<ul>
+    <li
+        v-for="item in nav.children?.filter((x) => x._path != nav._path)"
+        :key="item._path"
+    >
+        <template v-if="item.children">
+            <h2
+                class="
+                    text-sm
+                    font-bold
+                    leading-10
+                    tracking-tight
+                    text-gray-400
+                    border-b
+                    border-gray-200
+                    pb-2
+                    mb-2
+                    mt-10
+                "
+            >{{ item.title }}</h2>
+
+            <SiteCategory :nav="item" />
+        </template>
+
+        <template v-else>
+            <NuxtLink
+                :to="item._path"
+                class="
+                    flex
+                    w-full
+                    items-center
+                    text-left
+                    gap-x-2
+                    py-2
+                "
+            >
+                <DocumentTextIcon
+                    class="w-4 h-4 text-sky-500"
+                />
+                <span
+                    class="
+                        text-lg
+                        font-semibold
+                        leading-7
+                        text-gray-900
+                        hover:text-sky-400
+                    "
+                >{{ item.title }}</span>
+            </NuxtLink>
+        </template>
+    </li>
+</ul>
+</template>
+
+<script setup lang="ts">
+import { DocumentTextIcon } from '@heroicons/vue/24/outline'
+import type { NavItem } from '@nuxt/content/dist/runtime/types'
+
+defineProps<{
+    nav: NavItem
+}>()
+</script>
--- components/content/Category.vue
+++ components/content/Category.vue
@@ -1,69 +1,10 @@
 <template>
-<div class="not-prose space-y-10">
-    <div
-        v-if="navigation"
-        v-for="section in navigation.children"
-        :key="section.path"
-    >
-        <h2
-            v-if="navigation.showTitle"
-            class="
-                text-sm
-                font-bold
-                leading-10
-                tracking-tight
-                text-gray-400
-                border-b
-                border-gray-200
-                pb-2
-                mb-2
-            "
-        >{{ section.title }}</h2>
-        <ul>
-            <li
-                v-for="article in section.children"
-                :key="article.path"
-                class="
-                    flex
-                    w-full
-                    items-start
-                    justify-between
-                    text-left
-                    text-gray-900
-                "
-            >
-                <NuxtLink
-                    :to="article._path"
-                    class="
-                        flex
-                        w-full
-                        items-center
-                        text-left
-                        gap-x-2
-                        py-2
-                    "
-                >
-                    <DocumentTextIcon
-                        class="w-4 h-4 text-sky-500"
-                    />
-                    <span
-                        class="
-                            text-lg
-                            font-semibold
-                            leading-7
-                            text-gray-900
-                            hover:text-sky-400
-                        "
-                    >{{ article.title }}</span>
-                </NuxtLink>
-            </li>
-        </ul>
-    </div>
+<div class="not-prose">
+    <SiteCategory :nav="navigation" />
 </div>
 </template>
 
 <script setup lang="ts">
-import { DocumentTextIcon } from '@heroicons/vue/24/outline'
 import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
 
 const route = useRoute()
@@ -81,17 +22,8 @@ const query: QueryBuilderParams = {
     ],
 }
 
-const { data: navigation } = await useAsyncData(route.path, async () => {
-    const [ navigation ] = await fetchContentNavigation(query)
-    const [ category ] = navigation.children!
-
-    if(category._path == route.path) {
-        // category
-        category.showTitle = true
-        // remove self from children
-        category.children = category.children?.filter((item) => item._path != route.path)
-    }
-
-    return category
-})
+let [ navigation ] = await fetchContentNavigation(query)
+while(navigation && navigation._path != route.path && navigation.children) {
+    navigation = navigation.children[0]
+}
 </script>
