039f7e70e26a44a05030f8ec598e52ed26be03b1
Andrey Dyldin <and@cesbo.com>
2023-07-07 13:12:49 +0300

    icon for Note/Alert

--- components/content/Alert.vue
+++ components/content/Alert.vue
@@ -2,11 +2,23 @@
 <div
     class="
         ring-2 ring-inset ring-red-500
-        px-4 py-3
+        pl-12 pr-2 py-3
         rounded
         relative
+        overflow-hidden
+        shadow-sm
     "
 >
+    <ExclamationTriangleIcon
+        class="absolute left-3 top-[18px] w-6 h-6 text-red-500"
+    />
+
     <ContentSlot :use="$slots.default" unwrap="p" />
 </div>
 </template>
+
+<script setup lang="ts">
+import {
+    ExclamationTriangleIcon,
+} from '@heroicons/vue/24/solid'
+</script>
