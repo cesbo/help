3a0c22a71d52d94ccd1cde77aad255ccb4e5c115
Andrey Dyldin <and@cesbo.com>
2024-01-05 14:44:41 +0200

    move cesbo.com to app.cesbo.com

--- components/SiteMenu.vue
+++ components/SiteMenu.vue
@@ -65,7 +65,7 @@ const navigation = [
     { name: 'Home', to: 'https://www.cesbo.com' },
     { name: 'Docs', to: '/' },
     { name: 'Support', to: 'https://support.cesbo.com' },
-    { name: 'Pricing', to: 'https://cesbo.com/astra-license' },
-    { name: 'Profile', to: 'https://cesbo.com/accounts/login/?next=/accounts/profile/' },
+    { name: 'Pricing', to: 'https://app.cesbo.com/orders/software/astra/buy/' },
+    { name: 'Profile', to: 'https://app.cesbo.com/accounts/login/?next=/accounts/profile/' },
 ]
 </script>
