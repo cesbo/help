6dfc4908d2dfe0baefe8c839192be50bb4b9570c
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2025-04-27 21:30:06 +0200

    Get a first succesful translation

--- scripts/translate/index.js
+++ scripts/translate/index.js
@@ -3,14 +3,16 @@ import remarkParse from 'remark-parse'
 import { unified } from 'unified'
 import { removePosition } from 'unist-util-remove-position'
 
+const LIBRE_TRANSLATE_URL = 'http://103.249.134.120:10000'
+const PAGE_WITH_EN_DOCS = '../../src/content/docs/en'
+
 const processor = unified()
     .use(remarkParse)
 
 let page
 
 try {
-    page = fs.readFileSync('./pages/mpts-settings.md', 'utf8')
-    console.log(page)
+    page = fs.readFileSync(`${PAGE_WITH_EN_DOCS}/astra/delivery/mpts-settings.md`, 'utf8')
 } catch (err) {
     console.error(err)
 }
@@ -18,6 +20,22 @@ try {
 const parseTree = processor.parse(page)
 const tree = await processor.run(parseTree)
 
-removePosition(tree, { force: true })
+// removePosition(tree, { force: true })
+// console.dir(tree, { depth: null })
 
-console.dir(tree, { depth: null })
+const toTranslate = page.substring(0, 100)
+console.log(toTranslate)
+
+const res = await fetch(`${LIBRE_TRANSLATE_URL}/translate`, {
+	method: "POST",
+	body: JSON.stringify({
+		q: toTranslate,
+		source: "en",
+		target: "ru",
+		format: "text",
+		alternatives: 1
+	}),
+	headers: { "Content-Type": "application/json" }
+});
+
+console.log(await res.json());
