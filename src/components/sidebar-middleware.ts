dca9f032ad740ce0e01c9d88c258ce9b3a9b265a
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2025-05-01 20:53:56 +0200

    Show sidebar of only specific product

--- src/components/sidebar-middleware.ts
+++ src/components/sidebar-middleware.ts
@@ -2,26 +2,29 @@ import { defineRouteMiddleware } from '@astrojs/starlight/route-data'
 
 const products = ['astra', 'alta', 'senta', 'misc']
 
-export const onRequest = defineRouteMiddleware((context) => {
+export const onRequest = defineRouteMiddleware(async (context, next) => {
+    if (!context.locals.starlightRoute.hasSidebar) {
+        return
+    }
+
     const { url } = context.request
-    const { sidebar: originalSidebar } = context.locals.starlightRoute;
-    console.log("So url is: " + new URL(url).pathname)
 
     const currentProduct = new URL(url).pathname.split('/')[2]
-    console.log("So currentProduct is: " + currentProduct)
-
     if (!currentProduct) {
         return
     }
 
     const productIndex = products.findIndex((product) => {
-        return product.toLowerCase() === currentProduct.toLowerCase();
+        return product.toLowerCase() === currentProduct.toLowerCase()
     });
     if (productIndex === -1) {
         return
     }
-    console.log('productIndex', productIndex)
-    const productSidebar = originalSidebar[productIndex]
+
+    // Next middleware will configure sidebar labels
+    await next()
+
+    const productSidebar = context.locals.starlightRoute.sidebar[productIndex]
     // @ts-ignore
     context.locals.starlightRoute.sidebar = [...productSidebar.entries]
 })
